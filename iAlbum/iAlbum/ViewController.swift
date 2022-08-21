d1596d1e9c590a5bcda30ba72b523e0a56c22bae
Carlos Osejo <caosejo@gmail.com>
2022-08-20 18:16:37 -0600

    Project creation

---
+++ iAlbum/iAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iAlbum
+//
+//  Created by Carlos Osejo on 20/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
